3c900f48ffb9f44f7b878d92a212d595cb3b07b1
spocklogic <averykarlin3@gmail.com>
2016-10-10 16:18:48 -0400

    push

--- CompSystems/notes.asm
+++ CompSystems/notes.asm
@@ -33,6 +33,7 @@ END
 ;.ADDR sets the current memory address being set by the code for storing instructions/data in assembly
 ;Labels are put unindented before a line to signify that line being written as a label
 ;.Fill automatically does .ADDR + 1 after
+;.FALIGN moves to next multiple of 16
 ;CODE and DATA determine where can be written at any given time
 ;.BLKW moves the cursor to after the block, setting all to 0 in between
 ;Tabs don't matter, but new lines do
@@ -42,3 +43,23 @@ END
 
 ; STRINGZ adds a 0 character at the end to designate the end
 ;MOD not working
+
+;R1 vs r1 - both ok
+
+;PSR - 2:0 = NZP, 15 = Supervisor Mode
+;x0000-x1FFF - User Code
+;x2000-x3FFF - Global Variables (User-defined)
+;x4000-x6FFF - Dynamic Storage/Heap (User-defined)
+;x7000-x7FFF - Local Storage/Stack (User-defined)
+;x8000-x80FF - Trap Vector Table (Do Not Disturb)
+;x8200-xBFFF - OS (Do Not Disturb)
+;xC000-xFDFF - Video Output/Memory (Maps to pixels)
+;xFE00-xFFFF - I/O Device Register Addresses
+
+;CONST adds a signed value, (-FF-1, FF), HICONST adds xFF_00, removing the sign bit from the original
+
+;Script file assumes decimal, uses hex otherwise
+
+;= PC + 1 means incremented PC, not incremented twice
+
+;.CONST/UCONST replaced by assembler during assembly - LC is for constant, not for variable
