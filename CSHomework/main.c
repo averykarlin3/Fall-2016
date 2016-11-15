e67b63db42b401a1f2e484541e7a93c659afade8
Avery <Averykarlin3@gmail.com>
2016-11-14 20:37:11 -0500

    stuff

--- CSHomework/main.c
+++ CSHomework/main.c
@@ -1,5 +1,13 @@
 #include "lc4.h"
 
+int sext(word n, int len) {
+	if(n > pow(2, len - 1))
+		int mask = ~(pow(2, len) - 1);
+		return mask | n;
+	else	
+		return n;
+}
+
 int main() {
 	word inst = 0x0013;
 	printf("%u\n", sext(UIMM5(inst), 5));
