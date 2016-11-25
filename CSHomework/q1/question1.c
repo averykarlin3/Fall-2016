82be502070a00a2cf8cb562a3fcc3fee70f1609b
Avery <Averykarlin3@gmail.com>
2016-11-25 01:57:38 -0500

    Work

--- CSHomework/q1/question1.c
+++ CSHomework/q1/question1.c
@@ -6,3 +6,30 @@
 //Type in source code below
 //---------------------------------------------------------
 
+#include <math.h>
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int argc, char* argv[]) {
+	int n = 0;
+	int num = 0;
+	printf("Enter Number: ");
+	scanf("%i", &num);
+	int total = 0;
+	while(pow(2, n) < num) {
+		n++;
+	}
+	while(n >= 0) {
+		if(pow(2, n) <= num) {
+			num -= pow(2, n);
+			total++;
+		}
+		n--;
+	}
+	printf("Answer: %i\n", total);
+	return 0;
+}
+
+//Scanf doesn't end with \n
+//<> use printf and scanf as normal
+//Printf doesn't use \n before scanf
--- CSHomework/q2/question2.c
+++ CSHomework/q2/question2.c
@@ -6,3 +6,71 @@
 //Type in source code below
 //---------------------------------------------------------
 
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+typedef struct {
+	char name[50];
+	int row;
+	int column;
+} seat;
+
+int main() {
+	char input = 0;
+	int size = 0;
+	seat* plane = 0;
+	int fullSeat = 0;
+	while(1) {
+		printf("Choice (a,f,n,q): ");
+		scanf(" %c", &input);
+		switch(input) {
+			case 'n':
+				free(plane);
+				fullSeat = 0;
+				printf("Number of Seats: ");
+				scanf("%i", &size);
+				plane = (seat*) malloc(sizeof(seat) * size);
+				break;
+			case 'a':
+				if(fullSeat == size) {
+					printf("Airplane full\n");
+				}
+				else {
+					char name[50];
+					printf("Enter name: ");
+					scanf("%s", &name);
+					strncpy(plane[fullSeat].name, name, 50);
+					printf("Enter row: ");
+					scanf("%i", &(plane[fullSeat].row));
+					printf("Enter column: ");
+					scanf("%i", &(plane[fullSeat].column));
+					fullSeat++;
+				}
+				break;
+			case 'f':
+			printf("Here\n");
+			int searchColumn;
+			int searchRow;
+			printf("Enter row: ");
+			scanf("%i", &searchRow);
+			printf("Enter column: ");
+			scanf("%i", &searchColumn);
+			int found = 0;
+			for(int i = 0; i < size; i++) {
+				if(plane[i].row == searchRow && plane[i].row == searchColumn) {
+					printf("Name: %s\n", plane[i].name);
+					found = 1;
+				}
+			}
+			if(!found)
+				printf("Not found\n");
+			break;
+			case 'q':
+				return 0;
+		}
+	}
+}
+
+//Why is a space needed in scanf
+//When pointer vs not pointer with struct
